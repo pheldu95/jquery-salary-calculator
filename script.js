53c3036948f8ba9d97a9e4d43e6229bdc3f21e1d
Duncan Phelps <phelps.duncan@gmail.com>
2020-01-30 22:34:10 -0600

    now it doesn't try to add a string to the total monthly counter. incase the user inputs a string of letters.

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function readyNow(){
 
 let totalSalary = 0;
 //let employeeArray = [];
-
 function addEmployee(){
   let first = $ ('#firstNameInput').val();
   let last = $ ('#lastNameInput').val();
@@ -25,9 +24,9 @@ function addEmployee(){
   // };
   //employeeArray.push(employeeObject);
   $('#employeeTable').append(`<tr><td>${first}</td><td>${last}</td><td>${idNum}</td><td>${jobTitle}</td><td>${annualSal}</td><td><button id = "deleteButton">delete</button></th></tr>`);
-
-  annualSal = parseInt(annualSal);
-  totalSalary += annualSal;
-  $(".total").text(totalSalary);
-
+  if(isNaN(annualSal) === false){ //checks to see if the salary entered is a number or not. if it isn't a number, then nothing will be done with the total salary. if it is, then it will be added
+    annualSal = parseInt(annualSal);
+    totalSalary += annualSal;
+    $(".total").text(totalSalary);
+  }
 }
