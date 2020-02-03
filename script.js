c6db8a134ef66607adcd668d4ef19fbaae6173f3
Duncan Phelps <phelps.duncan@gmail.com>
2020-02-03 08:34:06 -0600

    everything works except it doesnt subtract the total cost if you delete an employee. need to work on that still.

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ $(document).ready(readyNow);
 
 function readyNow(){
   $('#submit').on('click', addEmployee);
+  $('#employeeTable').on('click', '#deleteButton', deleteRow);
 }
 
 let totalSalary = 0;
+//let rowNum = 0;
 //let employeeArray = [];
 function addEmployee(){
   let first = $ ('#firstNameInput').val();
@@ -12,7 +14,9 @@ function addEmployee(){
   let idNum = $ ('#idInput').val();
   let jobTitle = $ ('#titleInput').val();
   let annualSal = $ ('#annualSalaryInput').val();
-  if(  first.length === 0){ //makes it so the inputs are not appended to the dom if first name is left blank
+  if(  first.length === 0 || last.length === 0 ||
+    idNum.length === 0 || jobTitle.length === 0
+    || annualSal.length === 0){ //makes it so the inputs are not appended to the dom if first name is left blank
     return;
   }
   // let employeeObject = {
@@ -23,10 +27,22 @@ function addEmployee(){
   //   annualSalary: annualSal
   // };
   //employeeArray.push(employeeObject);
-  $('#employeeTable').append(`<tr><td>${first}</td><td>${last}</td><td>${idNum}</td><td>${jobTitle}</td><td>${annualSal}</td><td><button id = "deleteButton">delete</button></th></tr>`);
+  $('#employeeTable').append(`<tr id = rowNum><td>${first}</td><td>${last}</td><td>${idNum}</td><td>${jobTitle}</td><td id = 'annualSalID'>${annualSal}</td><td><button id = "deleteButton">delete</button></td></tr>`);
   if(isNaN(annualSal) === false){ //checks to see if the salary entered is a number or not. if it isn't a number, then nothing will be done with the total salary. if it is, then it will be added
     annualSal = parseInt(annualSal);
     totalSalary += annualSal;
     $(".total").text(totalSalary);
   }
+  //rowNum++;
+  //console.log(rowNum);
+}
+function deleteRow(){
+  let subtractSal = $(this).closest('tr').children('#annualSalID').val(); //trying to target the salary of the closest employee
+  console.log(subtractSal);
+  console.log(totalSalary);
+  totalSalary -= subtractSal;
+  console.log(totalSalary);
+  $(".total").text(totalSalary);
+  $(this).parent().parent().remove();
+
 }
