38e0d418a9886371ffe803204b6169346b3efebc
Duncan Phelps <phelps.duncan@gmail.com>
2020-02-03 09:21:56 -0600

    forgot to make it monthly salary instead of total annual salary. fixed it. Also, added dollar signs in.

--- script.js
+++ script.js
@@ -6,38 +6,30 @@ function readyNow(){
 }
 
 let totalSalary = 0;
-//let rowNum = 0;
-//let employeeArray = [];
+
 function addEmployee(){
   let first = $ ('#firstNameInput').val();
   let last = $ ('#lastNameInput').val();
   let idNum = $ ('#idInput').val();
   let jobTitle = $ ('#titleInput').val();
-  let annualSal = $ ('#annualSalaryInput').val();
+  let monthlySal = ($ ('#annualSalaryInput').val())/12;
   if(  first.length === 0 || last.length === 0 ||
     idNum.length === 0 || jobTitle.length === 0
-    || annualSal.length === 0){ //makes it so the inputs are not appended to the dom if first name is left blank
+    || monthlySal.length === 0){ //makes it so the inputs are not appended to the dom if first name is left blank
     return;
   }
-  // let employeeObject = {
-  //   firstName: first,
-  //   lastName: last,
-  //   id: idNum,
-  //   title: jobTitle,
-  //   annualSalary: annualSal
-  // };
+
   //employeeArray.push(employeeObject);
-  $('#employeeTable').append(`<tr id = rowNum><td>${first}</td><td>${last}</td><td>${idNum}</td><td>${jobTitle}</td><td id = 'annualSalID'>${annualSal}</td><td><button id = "deleteButton">delete</button></td></tr>`);
-  if(isNaN(annualSal) === false){ //checks to see if the salary entered is a number or not. if it isn't a number, then nothing will be done with the total salary. if it is, then it will be added
-    annualSal = parseInt(annualSal);
-    totalSalary += annualSal;
+  $('#employeeTable').append(`<tr id = rowNum><td>${first}</td><td>${last}</td><td>${idNum}</td><td>${jobTitle}</td><td id = 'monthlySalID'>$${monthlySal}</td><td><button id = "deleteButton">delete</button></td></tr>`);
+  if(isNaN(monthlySal) === false){ //checks to see if the salary entered is a number or not. if it isn't a number, then nothing will be done with the total salary. if it is, then it will be added
+    monthlySal = parseInt(monthlySal);
+    totalSalary += monthlySal;
     $(".total").text(totalSalary);
   }
-  //rowNum++;
-  //console.log(rowNum);
+
 }
 function deleteRow(){
-  let subtractSal = $(this).closest('tr').children('#annualSalID').val(); //trying to target the salary of the closest employee
+  let subtractSal = $(this).closest('tr').children('#monthlySalID').val(); //trying to target the salary of the closest employee
   console.log(subtractSal);
   console.log(totalSalary);
   totalSalary -= subtractSal;
